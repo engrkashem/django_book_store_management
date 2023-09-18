148227989849330c46926e5292c94b82f36aeddc
engrkashem <kashemaust@gmail.com>
2023-09-18 20:30:57 +0600

    crispy forms added

--- book/views.py
+++ book/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from book.forms import BookStoreForm
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'store_book.html')
+    return render(request, 'base.html')
+
+
+def store_book(request):
+    books = BookStoreForm(request.POST)
+    print(books)
+    return render(request, 'store_book.html', {'form': books})
