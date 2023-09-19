8a4383686678bb1a00348a751389059463566f8d
engrkashem <kashemaust@gmail.com>
2023-09-19 09:57:19 +0600

    admin panel edited to list display

--- book/admin.py
+++ book/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from book.models import BookStoreModel
 
 # Register your models here.
-admin.site.register(BookStoreModel)
+
+
+class BookStoreModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'book_name', 'author', 'category',
+                    'first_published', 'last_published')
+
+
+admin.site.register(BookStoreModel, BookStoreModelAdmin)
